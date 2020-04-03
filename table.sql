df547b11362c20db66f41b4086aa1988dc3b9d32
Muhan Li <mlfromchina@gmail.com>
2020-04-03 16:21:33 -0700

    populated tuples

--- table.sql
+++ table.sql
@@ -24,7 +24,7 @@ drop table Weapon_equip3 cascade constraints;
 
 
 CREATE TABLE General(
-	GID NUMBER primary key,
+    GID NUMBER primary key,
 	General_name varchar2(50),
 	General_rank varchar2(50)
 );
@@ -45,12 +45,12 @@ CREATE TABLE MilitaryUnit(
 CREATE TABLE Mission_takePlace_assign1(
     MissionID VARCHAR2(50) Primary Key, 
     Title VARCHAR2(50), 
-    StartDate DATE
+    StartDate VARCHAR2(50)
 );
 
 
 CREATE TABLE Mission_takePlace_assign2(
-    EndDate DATE Primary Key, 
+    EndDate VARCHAR2(50) Primary Key, 
     Mission_status VARCHAR2(50)
 );
 
@@ -59,7 +59,7 @@ CREATE TABLE Mission_takePlace_assign3(
     AreaID NUMBER, 
     GID NUMBER, 
     MUID NUMBER, 
-    EndDate DATE,
+    EndDate VARCHAR2(50),
     Primary Key(MissionID, AreaID, GID, MUID, EndDate), 
     Foreign Key(AreaID) References Area (AreaID) ON DELETE CASCADE,
     Foreign Key(GID) References General (GID) ON DELETE CASCADE,
@@ -75,9 +75,9 @@ CREATE TABLE MissionBudgetReport_record(
 	Foreign Key (MUID) References MilitaryUnit (MUID) ON DELETE CASCADE
 );
 CREATE TABLE MUBudgetReport_record(
-	MUID NUMBER,
 	Budget_year  NUMBER,
     Amount  NUMBER,
+	MUID NUMBER,
     Primary Key(MUID, Budget_year), 
 	Foreign Key (MUID) References MilitaryUnit (MUID) ON DELETE CASCADE
 );
@@ -97,7 +97,7 @@ CREATE TABLE Combatant(
 
 CREATE TABLE Commander(
 	CMID NUMBER primary key,
-	Commander_rank NUMBER, 
+	Commander_rank varchar2(50), 
     Foreign Key (CMID) References Combatant (CID) ON DELETE CASCADE
 );
 CREATE TABLE Soldier_enrolls(
@@ -173,23 +173,23 @@ insert into MilitaryUnit values(3,5,3,2);
 insert into MilitaryUnit values(4,10,4,0);
 insert into MilitaryUnit values(5,8,5,0);
 
-insert into Mission_takePlace_assign1 values(1,'Storm Operation',2020-01-03);
-insert into Mission_takePlace_assign1 values(2,'Code001',2017-06-08);
-insert into Mission_takePlace_assign1 values(3,'Rescue X',2018-03-15);
-insert into Mission_takePlace_assign1 values(4,'Black Sea Operation',2020-02-11);
-insert into Mission_takePlace_assign1 values(5,'BKSN',2019-12-26);
+insert into Mission_takePlace_assign1 values(1,'Storm Operation', '2020-01-03');
+insert into Mission_takePlace_assign1 values(2,'Code001', '2017-06-08');
+insert into Mission_takePlace_assign1 values(3,'Rescue X', '2018-03-15');
+insert into Mission_takePlace_assign1 values(4,'Black Sea Operation', '2020-02-11');
+insert into Mission_takePlace_assign1 values(5,'BKSN', '2019-12-26');
 
-insert into Mission_takePlace_assign2 values(NULL, 'IN_PROGRESS');
-insert into Mission_takePlace_assign2 values(2017-11-20,'COMPLETED');
-insert into Mission_takePlace_assign2 values(2018-03-18,'COMPLETED');
-insert into Mission_takePlace_assign2 values(NULL, 'IN_PROGRESS');
-insert into Mission_takePlace_assign2 values(NULL, 'IN_PROGRESS');
+insert into Mission_takePlace_assign2 values('2020-01-05', 'FAILED');
+insert into Mission_takePlace_assign2 values('2017-11-20','SUCCESS');
+insert into Mission_takePlace_assign2 values('2018-03-18','SUCCESS');
+insert into Mission_takePlace_assign2 values('2020-03-09', 'SUCCESS');
+insert into Mission_takePlace_assign2 values('2020-01-26', 'FAILED');
 
-insert into Mission_takePlace_assign3 values(1,1,1,5,NULL);
-insert into Mission_takePlace_assign3 values(2,5,4,2,2017-11-20);
-insert into Mission_takePlace_assign3 values(3,3,3,4,2018-03-18);
-insert into Mission_takePlace_assign3 values(4,4,5,3,NULL);
-insert into Mission_takePlace_assign3 values(5,2,2,1,NULL);
+insert into Mission_takePlace_assign3 values(1,1,1,5,'2020-01-05');
+insert into Mission_takePlace_assign3 values(2,5,4,2,'2017-11-20');
+insert into Mission_takePlace_assign3 values(3,3,3,4,'2018-03-18');
+insert into Mission_takePlace_assign3 values(4,4,5,3,'2020-03-09');
+insert into Mission_takePlace_assign3 values(5,2,2,1,'2020-01-26');
 
 insert into MissionBudgetReport_record values(1,20000,1000);
 insert into MissionBudgetReport_record values(2,50000,1001);
@@ -202,9 +202,77 @@ insert into MUBudgetReport_record values(2019,9000,2);
 insert into MUBudgetReport_record values(2018,4000,3);
 insert into MUBudgetReport_record values(2018,4500,4);
 insert into MUBudgetReport_record values(2017,8000,5);
-  
--- TODO: 
--- Populate Combantant table and onwards
+
+insert into Combatant values(1, 'Jayden Young', 'Healthy', 'Vancouver', 1.84, 76, 43, 10, 1);
+insert into Combatant values(2, 'Natasha Thompson', 'Healthy', 'Richmond', 1.7, 62, 45, 12, 2);
+insert into Combatant values(3, 'Rene Jarvis', 'Healthy', 'Mission', 1.76, 61, 32, 7, 3);
+insert into Combatant values(4, 'Leo Anderson', 'Healthy', 'Arkham', 1.9, 80, 37, 9, 4);
+insert into Combatant values(5, 'Journey Elliott', 'Healthy', 'Toronto', 1.82, 84, 38, 10, 5);
+insert into Combatant values(6, 'Harriet Gray', 'Healthy', 'London', 1.84, 83, 31, 8, 1);
+insert into Combatant values(7, 'Katelyn Dotson', 'Injured', 'Ottawa', 1.75, 65, 29, 6, 2);
+insert into Combatant values(8, 'Darrell Richmond', 'Healthy', 'Ottawa', 1.74, 69, 35, 5, 5);
+insert into Combatant values(9, 'Georgia Brown', 'Healthy', 'Kelowna', 1.76, 77, 36, 6, 3);
+insert into Combatant values(10, 'Emiliano Mullen', 'Injured', 'Vancouver', 1.88, 84, 27, 4, 4);
+
+insert into Commander values(1, 'Team Leader');
+insert into Commander values(2, 'Team Leader');
+insert into Commander values(3, 'Group Leader');
+insert into Commander values(4, 'Second Group Leader');
+insert into Commander values(5, 'Second Team Leader');
+
+insert into Soldier_enrolls values(6, 2);
+insert into Soldier_enrolls values(7, 1);
+insert into Soldier_enrolls values(8, 0);
+insert into Soldier_enrolls values(9, 0);
+insert into Soldier_enrolls values(10, 2);
+
+insert into Vehicle_has1 values('Krauss-Maffei Wegmann Maschinenbau Kiel', 'Army', 50, 5);
+insert into Vehicle_has1 values('Textron Marine and Land Systems', 'Airforce', 30, 4);
+insert into Vehicle_has1 values('Krauss-Maffei Wegmann Maschinenbau Kiel zwei', 'Army', 50, 5);
+insert into Vehicle_has1 values('Krauss-Maffei Wegmann Maschinenbau Kiel drei', 'Amry', 50, 5);
+insert into Vehicle_has1 values('AVIC I', 'Navy', 35, 3);
+insert into Vehicle_has1 values('AVIC II', 'Navy', 30, 3);
+insert into Vehicle_has1 values('Norinco', 'Airforce', 30, 5);
+insert into Vehicle_has1 values('MacDonald Dettwiler', 'Airforce', 30, 6);
+insert into Vehicle_has1 values('Colt Canada', 'Airforce', 45, 4);
+insert into Vehicle_has1 values('Arva Industries', 'Army', 45, 4);
+insert into Vehicle_has1 values('Aeryon Labs', 'Army', 120, 5);
+insert into Vehicle_has1 values('Rose Mons', 'Navy', 120, 3);
+insert into Vehicle_has1 values('Bombardier', 'Army', 120, 5);
+insert into Vehicle_has1 values('Glock', 'Amry', 150, 4);
+insert into Vehicle_has1 values('German Naval Group', 'Navy', 150, 4);
+
+insert into Vehicle_has2 values(1, 5);
+insert into Vehicle_has2 values(2, 4);
+insert into Vehicle_has2 values(3, 1);
+insert into Vehicle_has2 values(4, 3);
+insert into Vehicle_has2 values(5, 2);
+insert into Vehicle_has2 values(6, NULL);
+insert into Vehicle_has2 values(7, NULL);
+insert into Vehicle_has2 values(8, NULL);
+insert into Vehicle_has2 values(9, NULL);
+insert into Vehicle_has2 values(10, NULL);
+insert into Vehicle_has2 values(11, NULL);
+insert into Vehicle_has2 values(12, NULL);
+insert into Vehicle_has2 values(13, NULL);
+insert into Vehicle_has2 values(14, NULL);
+insert into Vehicle_has2 values(15, NULL);
+
+insert into Vehicle_has3 values(1, 'Krauss-Maffei Wegmann Maschinenbau Kiel', 'Army');
+insert into Vehicle_has3 values(2, 'Textron Marine and Land Systems', 'Airforce');
+insert into Vehicle_has3 values(3, 'Krauss-Maffei Wegmann Maschinenbau Kiel zwei', 'Army');
+insert into Vehicle_has3 values(4, 'Krauss-Maffei Wegmann Maschinenbau Kiel drei', 'Amry');
+insert into Vehicle_has3 values(5, 'AVIC I', 'Navy');
+insert into Vehicle_has3 values(6, 'AVIC II', 'Navy');
+insert into Vehicle_has3 values(7, 'Norinco', 'Airforce');
+insert into Vehicle_has3 values(8, 'MacDonald Dettwiler', 'Airforce');
+insert into Vehicle_has3 values(9, 'Colt Canada', 'Airforce');
+insert into Vehicle_has3 values(10, 'Arva Industries', 'Army');
+insert into Vehicle_has3 values(11, 'Aeryon Labs', 'Army');
+insert into Vehicle_has3 values(12, 'Rose Mons', 'Navy');
+insert into Vehicle_has3 values(13, 'Bombardier', 'Army');
+insert into Vehicle_has3 values(14, 'Glock', 'Amry');
+insert into Vehicle_has3 values(15, 'German Naval Group', 'Navy');
 
 insert into Weapon_equip1 values('United Technologies','M16A4',54,400,30,5.56);
 insert into Weapon_equip1 values('BAE System','AWM',85,1600,7,300);
@@ -224,38 +292,38 @@ insert into Weapon_equip1 values('ST Kinetics','PF-89',98,400,1,80);
 insert into Weapon_equip1 values('Armament Research and Development Establishment','RPG-2',96,200,1,82);
 insert into Weapon_equip1 values('Armament Research and Development Establishment','RPG-16',90,800,1,58.3);
 
--- insert into Weapon_equip2 values(1,1998,8,'Bad');
--- insert into Weapon_equip2 values(2,1996,14,'Good');
--- insert into Weapon_equip2 values(3,1984,5,'Good');
--- insert into Weapon_equip2 values(4,1995,4,'Excellent');
--- insert into Weapon_equip2 values(5,1961,3,'Good');
--- insert into Weapon_equip2 values(6,1983,6,'Bad');
--- insert into Weapon_equip2 values(7,1946,10,'Excellent');
--- insert into Weapon_equip2 values(8,1980,1,'Good');
--- insert into Weapon_equip2 values(9,1860,9,'Good');
--- insert into Weapon_equip2 values(10,1961,7,'Bad');
--- insert into Weapon_equip2 values(11,1927,11,'Good');
--- insert into Weapon_equip2 values(12,1942,15,'Good');
--- insert into Weapon_equip2 values(13,1861,13,'Good');
--- insert into Weapon_equip2 values(14,1970,2,'Bad');
--- insert into Weapon_equip2 values(15,1980,12,'Excellent');
--- insert into Weapon_equip2 values(16,1954,6,'Bad');
--- insert into Weapon_equip2 values(17,1968,9,'Good');
+insert into Weapon_equip2 values(1,1998,6,'Bad');
+insert into Weapon_equip2 values(2,1996,8,'Good');
+insert into Weapon_equip2 values(3,1984,8,'Good');
+insert into Weapon_equip2 values(4,1995,9,'Excellent');
+insert into Weapon_equip2 values(5,1961,8,'Good');
+insert into Weapon_equip2 values(6,1983,7,'Bad');
+insert into Weapon_equip2 values(7,1946,8,'Excellent');
+insert into Weapon_equip2 values(8,1980,6,'Good');
+insert into Weapon_equip2 values(9,1860,7,'Good');
+insert into Weapon_equip2 values(10,1961,9,'Bad');
+insert into Weapon_equip2 values(11,1927,10,'Good');
+insert into Weapon_equip2 values(12,1942,10,'Good');
+insert into Weapon_equip2 values(13,1861,8,'Good');
+insert into Weapon_equip2 values(14,1970,7,'Bad');
+insert into Weapon_equip2 values(15,1980,7,'Excellent');
+insert into Weapon_equip2 values(16,1954,6,'Bad');
+insert into Weapon_equip2 values(17,1968,9,'Good');
 
--- insert into Weapon_equip3 values(1,'United Technologies','M16A4');
--- insert into Weapon_equip3 values(2,'BAE System','AWM');
--- insert into Weapon_equip3 values(3,'BAE System','M249');
--- insert into Weapon_equip3 values(4,'Norico','QBZ95');
--- insert into Weapon_equip3 values(5,'Norico','RGB-7');
--- insert into Weapon_equip3 values(6,'Norico','Desert Eagle');
--- insert into Weapon_equip3 values(7,'Kalashnikov Concern');
--- insert into Weapon_equip3 values(8,'TsNIITochMash','AN94');
--- insert into Weapon_equip3 values(9,'Raytheon','Thompson');
--- insert into Weapon_equip3 values(10,'Raytheon','RPK');
--- insert into Weapon_equip3 values(11,'TsNIITochMash','DP-28');
--- insert into Weapon_equip3 values(12,'Tula Arms Plant','MG42','Type 69 RPG');
--- insert into Weapon_equip3 values(13,'ST Kinetics','Gatling');
--- insert into Weapon_equip3 values(14,'Empresa Nacional Santa Bárbara','Type 69 RPG');
--- insert into Weapon_equip3 values(15,'ST Kinetics','PF-89');
--- insert into Weapon_equip3 values(16,'Armament Research and Development Establishment','RPG-2');
--- insert into Weapon_equip3 values(17,'Armament Research and Development Establishment','RPG-16');  
+insert into Weapon_equip3 values(1,'United Technologies','M16A4');
+insert into Weapon_equip3 values(2,'BAE System','AWM');
+insert into Weapon_equip3 values(3,'BAE System','M249');
+insert into Weapon_equip3 values(4,'Norico','QBZ95');
+insert into Weapon_equip3 values(5,'Norico','RGB-7');
+insert into Weapon_equip3 values(6,'Norico','Desert Eagle');
+insert into Weapon_equip3 values(7,'Kalashnikov Concern','AK-47');
+insert into Weapon_equip3 values(8,'TsNIITochMash','AN94');
+insert into Weapon_equip3 values(9,'Raytheon','Thompson');
+insert into Weapon_equip3 values(10,'Raytheon','RPK');
+insert into Weapon_equip3 values(11,'TsNIITochMash','DP-28');
+insert into Weapon_equip3 values(12,'Tula Arms Plant','MG42');
+insert into Weapon_equip3 values(13,'ST Kinetics','Gatling');
+insert into Weapon_equip3 values(14,'Empresa Nacional Santa Bárbara','Type 69 RPG');
+insert into Weapon_equip3 values(15,'ST Kinetics','PF-89');
+insert into Weapon_equip3 values(16,'Armament Research and Development Establishment','RPG-2');
+insert into Weapon_equip3 values(17,'Armament Research and Development Establishment','RPG-16');  
